80644765537aa4b45ef29db8e294f51add3ec2b5
Mark Yoo <yjinhyuk@gmail.com>
2020-09-20 20:36:52 +0900

    Modified: apply useCallback with generic

--- WordRelay.tsx
+++ WordRelay.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useCallback, useRef } from 'react';
 
 const WordRelay = () => {
+  const [word, setWord] = useState('감자');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  const onSubmitForm = useCallback((e: React.FormEvent) => {
+
+  }, []);
+  
+  const onChange = useCallback<(e: React.ChangeEvent<HTMLInputElement>) => void>((e) => {
+    setValue(e.currentTarget.value);
+  }, []);
+
   return(
     <>
       <div>{word}</div>
       <form onSubmit={onSubmitForm}>
-        <input ref={inputEl} value={value} onChange={(e) => setValue(e.currentTarget.value)}>입력</input>
+        <input ref={inputEl} value={value} onChange={onChange}>입력</input>
       </form>
       <div>{result}</div>
     </>
