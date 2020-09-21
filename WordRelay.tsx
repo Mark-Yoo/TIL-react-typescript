306707cb8dfee19a98792f0c5da81743ad6ddef4
Mark Yoo <yjinhyuk@gmail.com>
2020-09-21 10:56:27 +0900

    Modified: WordRelay.tsx bug fixed

--- WordRelay.tsx
+++ WordRelay.tsx
@@ -8,22 +8,38 @@ const WordRelay = () => {
   const inputEl = useRef<HTMLInputElement>(null);
 
   const onSubmitForm = useCallback((e: React.FormEvent) => {
-
-  }, []);
+    e.preventDefault();
+    const input = inputEl.current;
+    if (word[word.length - 1] === value[0]) {
+      setResult('정답');
+      setWord(value);
+      setValue('');
+      if (input) {
+        input.focus();
+      }
+    } else {
+      setResult('오답');
+      setValue('');
+      if (input) {
+        input.focus();
+      }
+    }
+  }, [value, word]);
   
   const onChange = useCallback<(e: React.ChangeEvent<HTMLInputElement>) => void>((e) => {
     setValue(e.currentTarget.value);
   }, []);
 
-  return(
+  return (
     <>
       <div>{word}</div>
       <form onSubmit={onSubmitForm}>
-        <input ref={inputEl} value={value} onChange={onChange}>입력</input>
+        <input ref={inputEl} value={value} onChange={onChange}/>
+        <button>입력!</button>
       </form>
       <div>{result}</div>
     </>
   );
-}
+};
 
 export default WordRelay;
