b69b6333314015f0576adddc77014227de9e6e5b
Mark Yoo <yjinhyuk@gmail.com>
2020-10-05 19:49:02 +0900

    Modified: RespondCheck.tsx detail: states are set

--- RespondCheck.tsx
+++ RespondCheck.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import { useState, useRef, useCallback } from 'react';
 
 const RespondCheck = () => {
-
+  const [state, setState] = useState('waiting');
+  const [message, setMessage] = useState('클릭 후 시작');
+  const [result, setResult] = useState<number[]>([]);
+  const timeout = useRef<number>(null);
+  const startTime = useRef(0);
+  const endTime = useRef(0);
 };
 
 export default RespondCheck;
