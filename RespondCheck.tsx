a1059f8b0f49083530ace492e88157d1cf79d36a
Mark Yoo <yjinhyuk@gmail.com>
2020-10-06 20:04:14 +0900

    Modified: layout for RespondCheck.tsx is now added

--- RespondCheck.tsx
+++ RespondCheck.tsx
@@ -8,6 +8,22 @@ const RespondCheck = () => {
   const timeout = useRef<number>(null);
   const startTime = useRef(0);
   const endTime = useRef(0);
+
+  const renderAverage = () => {
+    return result.length === 0 ? null :
+      <>
+        <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+        <button onClick={onReset}>리셋</button>
+      </>
+  }
+  return (
+    <>
+      <div id="screen" className={state} onClick={onClickScreen}>
+      {message}
+      </div>
+      {renderAverage()}
+    </>
+  )
 };
 
 export default RespondCheck;
